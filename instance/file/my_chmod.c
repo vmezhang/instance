af7f6f749dfdd33db244db020aa7d8f3300dad0a
Zhang Rui <v.me-zr@live.cn>
2013-11-27 20:44:52 +0800

    some instances about Linux C

---
+++ instance/file/my_chmod.c
@@ -0,0 +1,60 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  my_chmod.c
+ *
+ *    Description:  改变文件访问权限
+ *
+ *        Version:  1.0
+ *        Created:  2013年11月27日 20时14分01秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  zhangrui, v.mezhang@gmail.com
+ *        Company:  Class 1107 of Computer Science and Technology
+ *
+ * =====================================================================================
+ */
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	int mode; 	// 权限
+	int mode_u; 	// 所有者权限
+	int mode_g; 	// 所属组权限
+	int mode_o; 	// 其他组的权限
+	char *path;
+
+	// 检查参数个数的合法性
+	if (argc < 3) {
+		printf("%s <mode num> <target file>\n", argv[0]);
+		exit(0);
+	}
+
+	// 获取命令行参数
+	mode = atoi(argv[1]);
+	if (mode > 777 || mode < 0) {
+		printf("mode num error!\n");
+		exit(0);
+	}
+	
+	mode_u = mode / 100;
+	mode_g = (mode - (mode_u * 100)) / 10;
+	mode_o = mode - (mode_u * 100) - (mode_g * 10);
+	mode = (mode_u * 8 * 8) + (mode_g * 8) + mode_o; 	// 八进制转换
+	path = argv[2];
+
+	if (chmod(path, mode) == -1) {
+		perror("chmod error");
+		exit(1);
+	}
+
+	return 0;
+}
+
+
